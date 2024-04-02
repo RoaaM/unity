8f94a8365b143e6f10372c8c670cff2a1b874c00
root <root>
2024-04-03 02:27:04 +0300

    add car unity

--- src/App.js
+++ src/App.js
@@ -14,75 +14,17 @@ const App = () => {
     takeScreenshot,
     unload,
   } = useUnityContext({
-    loaderUrl: "/unitybuild/crateclicker.loader.js",
-    dataUrl: "/unitybuild/crateclicker.data",
-    frameworkUrl: "/unitybuild/crateclicker.framework.js",
-    codeUrl: "/unitybuild/crateclicker.wasm",
+    loaderUrl: "/unitybuild/bu.loader.js",
+    dataUrl: "/unitybuild/bu.data",
+    frameworkUrl: "/unitybuild/bu.framework.js",
+    codeUrl: "/unitybuild/bu.wasm",
     webglContextAttributes: {
       preserveDrawingBuffer: true,
     },
   });
 
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [screenshotDatas, setScreenshotDatas] = useState([]);
-  const [scores, setScores] = useState([]);
-
-  const handleClickStartGame = (time) => {
-    if (isLoaded === false || isPlaying === true) {
-      return;
-    }
-    setIsPlaying(true);
-    sendMessage("GameController", "StartGame", time);
-  };
-
-  const handleClickFullscreen = () => {
-    if (isLoaded === false) {
-      return;
-    }
-    requestFullscreen(true);
-  };
-
-  const handleClickScreenshot = () => {
-    if (isLoaded === false) {
-      return;
-    }
-    const screenshotData = takeScreenshot();
-    if (screenshotData !== undefined) {
-      setScreenshotDatas([screenshotData, ...screenshotDatas]);
-    }
-  };
-
-  const handleClickUnload = async () => {
-    if (isLoaded === false) {
-      return;
-    }
-    try {
-      await unload();
-      console.log("Unload success");
-    } catch (error) {
-      console.error(`Unable to unload: ${error}`);
-    }
-  };
-
-  const handleGameOver = useCallback(
-    (time, score) => {
-      time = Math.round(time);
-      setIsPlaying(false);
-      setScores([[time, score], ...scores]);
-    },
-    [scores]
-  );
-
-  useEffect(() => {
-    addEventListener("GameOver", handleGameOver);
-    return () => {
-      removeEventListener("GameOver", handleGameOver);
-    };
-  }, [handleGameOver, addEventListener, removeEventListener]);
-
   return (
     <div className={styles.container}>
-      <h1>Crate Clicker!</h1>
       <div className={styles.unityWrapper}>
         {isLoaded === false && (
           <div className={styles.loadingBar}>
@@ -96,31 +38,6 @@ const App = () => {
           unityProvider={unityProvider}
           style={{ display: isLoaded ? "block" : "none" }}
         />
-      </div>
-      <div className="buttons">
-        <button onClick={() => handleClickStartGame(5)}>
-          Start Short Game
-        </button>
-        <button onClick={() => handleClickStartGame(10)}>
-          Start Long Game
-        </button>
-        <button onClick={handleClickFullscreen}>Fullscreen</button>
-        <button onClick={handleClickScreenshot}>Screenshot</button>
-        <button onClick={handleClickUnload}>Unload</button>
-      </div>
-      <h2>Scores</h2>
-      <ul>
-        {scores.map(([time, score]) => (
-          <li key={time}>
-            {score} points with {time} seconds left!
-          </li>
-        ))}
-      </ul>
-      <h2>Screenshots</h2>
-      <div className={styles.screenshots}>
-        {screenshotDatas.map((data, index) => (
-          <img width={250} key={index} src={data} alt="Screenshot" />
-        ))}
       </div>
     </div>
   );
